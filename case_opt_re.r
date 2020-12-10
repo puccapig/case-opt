49f94d6a65c7b202486f0aece332dcdb27726aa5
Rachel Yuan Nong Mikkelsen <puccapig@gmail.com>
2020-12-10 13:46:10 +0100

    Add files via upload

---
+++ case_opt_re.r
@@ -0,0 +1,42 @@
+#Rachel Yuan Nong 2019-DEC-06
+#Human genes to Mouse_ Absolutely not recommeneded. 
+#2020-OCT-20 copied and modified for Mouse_ to Human genes.
+
+
+print("human genes_ < mouse using: genes_more ...")
+genesmore <- readline()
+genes_more <- read.csv(genesmore, sep = ";")
+
+#that is a file resulted from look_up_genes_names.R
+
+genes_less <- genes_more[,1]
+
+more_genes <- c(1:length(genes_less))
+
+case2 <- c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+
+case1 <- c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+
+case1 <- as.data.frame(case1)
+case2 <- as.data.frame(case2)
+
+for(i in 1:length(genes_less)){
+	a <- as.character(genes_less[i])
+	b <- strsplit(a, "")
+	b <- unlist(b)
+	new_b <- b[1]
+	ex_new_b <- length(b)-1
+
+		for(m in 1:ex_new_b){
+			m_b <- m + 1
+			if(match(b[m_b], case1[,1], nomatch = 0) == 0){
+				b_added <- b[m_b]}
+			if(match(b[m_b], case1[,1], nomatch = 0) != 0){
+				b_added <- case2[match(b[m_b], case1[,1], nomatch = 0), 1]}
+			new_b <- paste0(new_b, b_added)
+			}
+	more_genes[i] <- new_b
+}
+
+more_genes <- sapply(strsplit(more_genes, ","), function(x){x[2]})
+print("Returning in more_genes")
